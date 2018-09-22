26571ffff3f64294fab61f7cd35b805810ec572a
Diane <d.bunod@gmail.com>
2018-09-21 18:40:08 -0700

    v3

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,18 @@
+class UserController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    #User.create(name: params[],)
+
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+
+
+
+  root to: 'pages#index'
+  get '/startups', to: 'pages#startups', as: 'startups'
+  get '/investment', to: 'pages#investment'
+  get '/use-case', to: 'pages#use-case'
+
+  resources :users
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
